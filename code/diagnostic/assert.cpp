9c3993d255b51327e16ee47dfc58659a5163b5c0
Ice <>
2021-08-23 00:17:16 +0000

    Changes

---
+++ code/diagnostic/assert.cpp
@@ -0,0 +1,4 @@
+#include"diagnostic/assert.hpp"
+
+#include<iostream>
+void Diagnostic::a() { std::cout << "aaa\n";}
---
+++ code/diagnostic/assert.hpp
@@ -0,0 +1,8 @@
+#ifndef INCLUDED_ASSERT_HPP
+#define INCLUDED_ASSERT_HPP
+
+namespace Diagnostic {
+    void a();
+}
+
+#endif
