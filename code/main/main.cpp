88f5b8856677bbd5ce98cc67e99ca61d05bbfa67
Ice <>
2021-08-25 14:20:45 +0000

    Changes

---
+++ code/main/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include"concurrency/main_thread.hpp"
+#include"graphics/window.hpp"
+
+int main() {
+    Concurrency::main_thread_id = std::this_thread::get_id();
+    
+    // TODO: std::format
+    
+    // glfwGetVersionString() may be called before glfwInit()
+    std::cout << "GLFW version: " << glfwGetVersionString() << std::endl;
+
+    Graphics::Window w1(400, 400);
+    bool exit = false;
+
+    w1.add_event_callback([&](Events::Graphics::Window::Close const& _event) {
+	(void)_event;
+	std::cout << "Window closed" << std::endl;
+	exit = true;
+    });
+
+    w1.add_event_callback([&](Events::Graphics::Window::Resize const& _event) {
+	std::cout << "Window resized: " << _event.width << "x" << _event.height << std::endl;
+    });
+    
+    while(!exit) {
+	w1.await_events();
+    }
+
+}
+
+
+
